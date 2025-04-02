ea02cbae0b70142210a337b615bc35ee89dfb518
JonahPi <jaegerhuf-abbyy@yahoo.de>
2025-04-02 22:05:32 +0200

    test

--- src/secrets.h
+++ src/secrets.h
@@ -1,12 +1,12 @@
 #pragma once
 
 // Wi-Fi credentials
-#define WIFI_SSID "YOURssid"
-#define WIFI_PASSWORD "YOURpassword"
+#define WIFI_SSID "Yssid"
+#define WIFI_PASSWORD "Ypassword"
 
 // MQTT credentials
 #define MQTT_SERVER "io.adafruit.com"
 #define MQTT_PORT 1883
-#define MQTT_USER "YOURusername"
+#define MQTT_USER "Yusername"
 #define MQTT_PASS "YOURmqttpassword"
 #define MQTT_FEED_POSITION "YOURuser/feeds/stepper.position"
