7f6257da9e615636dd8137863fb0fa09e206597c
JonahPi <jaegerhuf-abbyy@yahoo.de>
2025-04-02 22:03:49 +0200

    Create secrets.h demo

---
+++ src/secrets.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Wi-Fi credentials
+#define WIFI_SSID "YOURssid"
+#define WIFI_PASSWORD "YOURpassword"
+
+// MQTT credentials
+#define MQTT_SERVER "io.adafruit.com"
+#define MQTT_PORT 1883
+#define MQTT_USER "YOURusername"
+#define MQTT_PASS "YOURmqttpassword"
+#define MQTT_FEED_POSITION "YOURuser/feeds/stepper.position"
