e59cbd76ee9d71cc5c01e887e08534c07c7750fc
JonahPi <jaegerhuf-abbyy@yahoo.de>
2025-04-02 22:16:09 +0200

    secrets dummy file

---
+++ src/secrets.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Wi-Fi credentials
+#define WIFI_SSID "YOUR_ssid"
+#define WIFI_PASSWORD "YOUR_password"
+
+// MQTT credentials
+#define MQTT_SERVER "io.adafruit.com"
+#define MQTT_PORT 1883
+#define MQTT_USER "YOUR_username"
+#define MQTT_PASS "YOUR_mqttpassword"
+#define MQTT_FEED_POSITION "YOUR_username/feeds/stepper.position"
